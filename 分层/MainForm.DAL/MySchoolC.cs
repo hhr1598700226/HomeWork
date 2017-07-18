7828a051217a2898af05fb9c8f1de18a79a52ea9
胡慧然 <1598700226@qq.com>
2017-07-18 09:56:40 +0800

    1

---
+++ 分层/MainForm.DAL/MySchoolC.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySchool.Model;
+using System.Data;
+
+namespace MainForm.DAL
+{
+
+    public class MySchoolC
+    {
+        public static readonly string strcon = "data source=.;database = myschool;user id = sa;pwd = 1";
+
+        public static SqlDataReader ExecuteReader(string sqlcmd)
+        {
+            SqlConnection sqlcon = new SqlConnection(strcon);
+            sqlcon.Open();
+            SqlCommand cmd = new SqlCommand(sqlcmd,sqlcon);
+            SqlDataReader Reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            return Reader;
+        }
+
+        public static int ExecuteScalar(string sqlcmd)
+        {
+            SqlConnection sqlcon = new SqlConnection(strcon);
+            sqlcon.Open();
+            SqlCommand cmd = new SqlCommand(sqlcmd,sqlcon);
+            int result = Convert.ToInt32(cmd.ExecuteScalar());
+            sqlcon.Close();
+            return result;
+        }
+
+        public static int ExecuteNonQuery(string sqlcmd)
+        {
+            SqlConnection sqlcon = new SqlConnection(strcon);
+            sqlcon.Open();
+            SqlCommand cmd = new SqlCommand(sqlcmd,sqlcon);
+            int result = cmd.ExecuteNonQuery();
+            sqlcon.Close();
+            return result;
+        }
+
+        public static Dictionary<int, string> GetGrade()
+        {
+            string sqlcmd = "select GradeId,GradeName from Grade";
+            SqlDataReader Reader = MySchoolC.ExecuteReader(sqlcmd);
+            Dictionary<int, string> dc = new Dictionary<int, string>();
+            while (Reader.Read())
+            {
+                dc.Add(Convert.ToInt32(Reader["Gradeid"]),Reader["GradeName"].ToString());
+            }
+            Reader.Close();
+            return dc;
+        }
+
+        public static List<Student> GetStudent()
+        {
+            string sqlcmd = @"select studentno,studentname,loginpwd,gender,gradeid,
+                            email,[address],Phone,borndate,IdentityCard 
+                            from student";
+            SqlDataReader Reader = MySchoolC.ExecuteReader(sqlcmd);
+            List<Student> DS = new List<Student>() { };
+            while (Reader.Read())
+            {
+                Student xxs = new Student();
+                xxs.StudentName = Reader["StudentName"].ToString();
+                xxs.StudentNo = Convert.ToInt32(Reader["StudentNo"]);
+                xxs.StudentPwd = Reader["LoginPwd"].ToString();
+                xxs.Gender = Reader["Gender"].ToString();
+                xxs.Grade = Reader["Gradeid"].ToString();
+                xxs.Email = Reader["Email"].ToString();
+                xxs.Address = Reader["Address"].ToString();
+                xxs.Phone = Reader["Phone"].ToString();
+                xxs.BornDate = Convert.ToDateTime(Reader["BornDate"]);
+                xxs.IdCard = Reader["IdentityCard"].ToString();
+                //Student xxs = new Student()
+                //{
+                //    StudentNo = Convert.ToInt32(Reader["StudentNo"]),
+                //    StudentPwd = Reader["LoginPwd"].ToString(),
+                //    StudentName = Reader["StudentName"].ToString(),
+                //    Address = Reader["Address"].ToString(),
+                //    BornDate = Convert.ToDateTime(Reader["BornDate"]),
+                //    Email = Reader["Email"].ToString(),
+                //    Gender = Reader["Gender"].ToString(),
+                //    Grade = Reader["GradeId"].ToString(),
+                //    IdCard = Reader["IdentityCard"].ToString(),
+                //    Phone = Convert.ToInt32(Reader["Phone"])
+                //};
+
+                DS.Add(xxs);
+            }
+            Reader.Close();
+            return DS;
+        }
+
+        public static List<Student> GetStudent(string str)
+        {
+            string sqlcmd = string.Format(@"select studentno,studentname,loginpwd,gender,gradeid,
+                            email,[address],Phone,borndate,IdentityCard 
+                            from student where gradeid = '{0}'",str);
+            SqlDataReader Reader = MySchoolC.ExecuteReader(sqlcmd);
+            List<Student> DS = new List<Student>() { };
+            while (Reader.Read())
+            {
+                Student xxs = new Student();
+                xxs.StudentName = Reader["StudentName"].ToString();
+                xxs.StudentNo = Convert.ToInt32(Reader["StudentNo"]);
+                xxs.StudentPwd = Reader["LoginPwd"].ToString();
+                xxs.Gender = Reader["Gender"].ToString();
+                xxs.Grade = Reader["Gradeid"].ToString();
+                xxs.Email = Reader["Email"].ToString();
+                xxs.Address = Reader["Address"].ToString();
+                xxs.Phone = Reader["Phone"].ToString();
+                xxs.BornDate = Convert.ToDateTime(Reader["BornDate"]);
+                xxs.IdCard = Reader["IdentityCard"].ToString();
+
+                DS.Add(xxs);
+            }
+            Reader.Close();
+            return DS;
+        }
+    }
+}
---
+++ 分层/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnselect = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btnselect
+            // 
+            this.btnselect.Location = new System.Drawing.Point(158, 10);
+            this.btnselect.Name = "btnselect";
+            this.btnselect.Size = new System.Drawing.Size(75, 23);
+            this.btnselect.TabIndex = 2;
+            this.btnselect.Text = "查找";
+            this.btnselect.UseVisualStyleBackColor = true;
+            this.btnselect.Click += new System.EventHandler(this.btnselect_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(372, 265);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 337);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnselect);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnselect;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
